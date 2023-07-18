1b76a38d7ea6b4a8a289764113caf706821444b8
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-19 01:44:34 +0300

    Complete Optional Functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (array2){
+    return array2.slice(array2.length-2, array2.length )
+}
+const selectingDrivers = new Array( returnFirstTwoDrivers,returnLastTwoDrivers)
+
+function createFareMultiplier (int){
+    return function(fare){
+        return fare * int
+    }
+}
+const fareDoubler = function(returnVal){
+    return returnVal * 2
+}
+
+const fareTripler = function(value){
+    return value * 3
+}
+function selectDifferentDrivers (drivers, arg2){
+    
+    if (arg2 === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers)
+    }
+    else if ( arg2 === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)}
+}
